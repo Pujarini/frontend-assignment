459b5c43092d2cb233acb3d31d2da7d6ea4c4ab8
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2024-12-22 23:05:22 +0530

    styling and layout fixes

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,15 +1,6 @@
 import React, { memo, useMemo } from "react";
 
 function Pagination({ totalPages, currentPage, onPageChange }) {
-    const maxVisiblePages = 10;
-
-
-    const pageNumbers = useMemo(() => {
-        const startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
-        const endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
-        return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
-
-    }, [currentPage, totalPages, maxVisiblePages])
 
 
     return <nav className="pagination" aria-label="Pagination navigation"
@@ -24,17 +15,7 @@ function Pagination({ totalPages, currentPage, onPageChange }) {
             Previous
         </button>
 
-        <p>{currentPage} of {totalPages}</p>
-
-        {/* {pageNumbers.map((number) => (
-            <button
-                key={number}
-                onClick={() => onPageChange(number)}
-                className={number === currentPage ? "active" : ""}
-            >
-                {number}
-            </button>
-        ))} */}
+        <span>{currentPage} of {totalPages}</span>
 
         <button
             onClick={() => onPageChange(currentPage + 1)}
